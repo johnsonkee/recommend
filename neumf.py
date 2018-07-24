e54e6b7af6e0cd2b2d8d09ea4822e542ec1f1672
wxz <876688461@qq.com>
2018-07-24 13:19:49 +0800

    change embedding to dense

--- neumf.py
+++ neumf.py
@@ -1,56 +1,64 @@
 import numpy as np
 from mxnet.gluon import nn
 from mxnet import nd
+import mxnet
 
 # in mxnet ,using mxnet.gluon.nn.Block to initiate the the deep neural network
 class NeuMF(nn.Block): #if using nn.Hybridblock, it will generate static graph
     def __init__(self, nb_users, nb_items,
                  mf_dim, mf_reg,
-                 mlp_layer_sizes, mlp_layer_regs):  # mlp_layer_regs is a reconfirm
+                 mlp_layer_sizes,
+                 mlp_layer_regs,# mlp_layer_regs is a reconfirm
+                 ctx):  # Indicate the context is CPU or GPU
         if len(mlp_layer_sizes) != len(mlp_layer_regs):
             raise RuntimeError('u dummy, layer_sizes != layer_regs!')
         if mlp_layer_sizes[0] % 2 != 0:
             raise RuntimeError('u dummy, mlp_layer_sizes[0] % 2 != 0')
         super(NeuMF, self).__init__()
-        nb_mlp_layers = len(mlp_layer_sizes)  # nb is the short of  number
+        with self.name_scope():
 
-        # TODO: regularization?
-        # TODO 1: problem：Using embedding or Dense
-        # the usage of nn.Embedding: nn.Embedding(num of input neuron, num of output neuron)
-        # the usage of nn.Dense: nn.Dense(num of output neuron, activation = 'relu'), while the num of input neurons
-        # can be ignored.
-        self.mf_user_embed = nn.Embedding(nb_users, mf_dim)
-        self.mf_item_embed = nn.Embedding(nb_items, mf_dim) # mf_dim means the number of predictive factors,
-        self.mlp_user_embed = nn.Embedding(nb_users, mlp_layer_sizes[0] // 2)  # put user and item into the mlp together
-        self.mlp_item_embed = nn.Embedding(nb_items, mlp_layer_sizes[0] // 2)
+            nb_mlp_layers = len(mlp_layer_sizes)  # nb is the short of  number
 
-        self.mlp = nn.Sequential()
-        for i in range(1, nb_mlp_layers):
-            self.mlp.add([nn.Dense(mlp_layer_sizes[i])])  # noqa: E501 # previous one is in
-                                                                                                   # latter one is out
-        self.final = nn.Embedding(mlp_layer_sizes[-1] + mf_dim, 1) # the final fully-connected layer
-        self.mf_user_embed.weight.data = nd.random.normal(0., 0.01, self.mf_user_embed.weight.shape)
-        self.mf_item_embed.weight.data = nd.random.normal(0., 0.01, self.mf_item_embed.weight.shape)
-        self.mlp_user_embed.weight.data = nd.random.normal(0., 0.01, self.mlp_user_embed.weight.shape)
-        self.mlp_item_embed.weight.data = nd.random.normal(0., 0.01, self.mlp_item_embed.weight.shape)
-        # golorot_uniform is one of a initialization, always used in fc layers, convolution layers, preventing its
-        # saturation
-        def golorot_uniform(layer):
-            # TODO 2: what is layer.in_feature
-            fan_out, fan_in = layer.weight.shape  #
-            limit = np.sqrt(6. / (fan_in + fan_out))
-            layer.weight.data = nd.uniform(-limit, limit)
+            # TODO: regularization?
+            # TODO 1: problem：Using embedding or Dense
+            # the usage of nn.Embedding: nn.Embedding(num of input neuron, num of output neuron)
+            # the usage of nn.Dense: nn.Dense(num of output neuron, activation = 'relu'), while the num of input neurons
+            # can be ignored.
+            self.mf_user_embed = nn.Dense(units=mf_dim, in_units=nb_users)
+            self.mf_item_embed = nn.Dense(units=mf_dim, in_units=nb_items) # mf_dim means the number of predictive factors,
+            self.mlp_user_embed = nn.Dense(units=mlp_layer_sizes[0] // 2, in_units=nb_users)  # put user and item into the mlp together
+            self.mlp_item_embed = nn.Dense(in_units=nb_items, units=mlp_layer_sizes[0] // 2)
 
-        def lecunn_uniform(layer):
-            fan_out, fan_in = layer.weight.shape  # noqa: F841, E501
-            limit = np.sqrt(3. / fan_in)
-            layer.weight.data = nd.uniform(-limit, limit)
+            self.mlp = nn.Sequential()
+            for i in range(1, nb_mlp_layers):
+                self.mlp.add([nn.Dense(units=mlp_layer_sizes[i],in_units=mlp_layer_sizes[i-1])])  # noqa: E501 # previous one is in
+                                    # latter one is out
+            self.final = nn.Dense(in_units=mlp_layer_sizes[-1] + mf_dim, units=1) # the final fully-connected layer
 
-        for layer in self.mlp:
-            if type(layer) != nn.Dense:
-                continue
-            golorot_uniform(layer)
-        lecunn_uniform(self.final)
+            # initialization
+            self.mf_user_embed.weight.data = nd.random.normal(0., 0.01, self.mf_user_embed.weight.shape)
+            self.mf_item_embed.weight.data = nd.random.normal(0., 0.01, self.mf_item_embed.weight.shape)
+            self.mlp_user_embed.weight.data = nd.random.normal(0., 0.01, self.mlp_user_embed.weight.shape)
+            self.mlp_item_embed.weight.data = nd.random.normal(0., 0.01, self.mlp_item_embed.weight.shape)
+            # golorot_uniform is one of a initialization, always used in fc layers, convolution layers, preventing its
+            # saturation
+            def golorot_uniform(layer):
+                # TODO 2: what is layer.in_feature
+                fan_out, fan_in = layer.weight.shape  #
+                limit = np.sqrt(6. / (fan_in + fan_out))
+                layer.weight.data = nd.uniform(-limit, limit)
+
+            def lecunn_uniform(layer):
+                fan_out, fan_in = layer.weight.shape  # noqa: F841, E501
+                limit = np.sqrt(3. / fan_in)
+                layer.weight.data = nd.uniform(-limit, limit)
+
+            for layer in self.mlp:
+                if type(layer) != nn.Dense:
+                    continue
+                golorot_uniform(layer)
+
+            lecunn_uniform(self.final)
 
     def forward(self, user, item, sigmoid=False):
         xmfu = self.mf_user_embed(user)  # user vector in matrix factorization
