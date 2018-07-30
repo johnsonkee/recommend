c6292fe5a93fb95dddeb0808da098ed5f30bf6f8
wxz <876688461@qq.com>
2018-07-30 14:06:08 +0800

    correct the neumf's initialize, forward function, it is a import change

--- neumf.py
+++ neumf.py
@@ -19,28 +19,37 @@ class NeuMF(nn.Block): #if using nn.Hybridblock, it will generate static graph
 
             nb_mlp_layers = len(mlp_layer_sizes)  # nb is the short of  number
 
-            # TODO: regularization?
-            # TODO 1: problem：Using embedding or Dense
+            # TODO: regularization
             # the usage of nn.Embedding: nn.Embedding(num of input neuron, num of output neuron)
-            # the usage of nn.Dense: nn.Dense(num of output neuron, activation = 'relu'), while the num of input neurons
+            # the usage of nn.Dense: nn.Dense(num of output neuron, activation = 'relu'),
+            # while the num of input neurons
             # can be ignored.
-            self.mf_user_embed = nn.Dense(units=mf_dim, in_units=nb_users)
-            self.mf_item_embed = nn.Dense(units=mf_dim, in_units=nb_items) # mf_dim means the number of predictive factors,
-            self.mlp_user_embed = nn.Dense(units=mlp_layer_sizes[0] // 2, in_units=nb_users)  # put user and item into the mlp together
-            self.mlp_item_embed = nn.Dense(in_units=nb_items, units=mlp_layer_sizes[0] // 2)
+            self.mf_user_embed = nn.Dense(units=mf_dim, in_units=nb_users,
+                                          weight_initializer=mx.init.Normal())
+            self.mf_item_embed = nn.Dense(units=mf_dim, in_units=nb_items,
+                                          weight_initializer=mx.init.Normal())
+                                        # mf_dim means the number of predictive factors,
+            self.mlp_user_embed = nn.Dense(units=mlp_layer_sizes[0] // 2, in_units=nb_users,
+                                           weight_initializer=mx.init.Normal())
+            # put user and item into the mlp together
+            self.mlp_item_embed = nn.Dense(in_units=nb_items, units=mlp_layer_sizes[0] // 2,
+                                           weight_initializer=mx.init.Normal())
 
             self.mlp = nn.Sequential()
             for i in range(1, nb_mlp_layers):
-                self.mlp.add(nn.Dense(units=mlp_layer_sizes[i],in_units=mlp_layer_sizes[i-1],activation='relu'))
-            self.final = nn.Dense(in_units=mlp_layer_sizes[-1] + mf_dim, units=1) # the final fully-connected layer
+                self.mlp.add(nn.Dense(units=mlp_layer_sizes[i],
+                                      in_units=mlp_layer_sizes[i-1],
+                                      activation='relu',
+                                      weight_initializer=mx.init.Xavier()))
+            self.final = nn.Dense(in_units=mlp_layer_sizes[-1] + mf_dim,
+                                  units=1,
+                                  weight_initializer=mx.init.Xavier())
+                                  # the final fully-connected layer
 
             # initialization
-            self.mf_user_embed.weight.data = nd.random.normal(0., 0.01, self.mf_user_embed.weight.shape, ctx=ctx)
-            self.mf_item_embed.weight.data = nd.random.normal(0., 0.01, self.mf_item_embed.weight.shape, ctx=ctx)
-            self.mlp_user_embed.weight.data = nd.random.normal(0., 0.01, self.mlp_user_embed.weight.shape, ctx=ctx)
-            self.mlp_item_embed.weight.data = nd.random.normal(0., 0.01, self.mlp_item_embed.weight.shape, ctx=ctx)
             # golorot_uniform is one of a initialization, always used in fc layers, convolution layers, preventing its
             # saturation
+            """
             def golorot_uniform(layer, ctx):
                 # TODO 2: what is layer.in_feature
                 fan_out, fan_in = layer.weight.shape
@@ -58,6 +67,7 @@ class NeuMF(nn.Block): #if using nn.Hybridblock, it will generate static graph
                 golorot_uniform(layer, ctx)
 
             lecunn_uniform(self.final,ctx)
+            """
 
     def forward(self, user, item, sigmoid=False):
         xmfu = self.mf_user_embed(user)  # user vector in matrix factorization
@@ -70,11 +80,11 @@ class NeuMF(nn.Block): #if using nn.Hybridblock, it will generate static graph
         # previous one: mlp = torch.cat((xmlpu, xmlpi), dim=1)
         # use nd.concatenate([xmlpu, xmlpi], axis=0) to replace
         xmlp = nd.concatenate([xmlpu, xmlpi], axis=1)
-        for i, layer in enumerate(self.mlp):
-            xmlp = layer(xmlp)
-            xmlp = nd.relu(xmlp)
+        # axis=0 means increasing the row, while axis=1 means increasing the column
 
-        x = nd.concatenate([xmf, xmlp], axis=0)
+        xmlp = self.mlp(xmlp)
+
+        x = nd.concatenate([xmf, xmlp], axis=1)
         x = self.final(x)
         if sigmoid:
             x = nd.sigmoid(x)
@@ -88,10 +98,9 @@ def main():
                   mlp_layer_regs=[0. for i in [256,128,64]],
                   ctx=mx.cpu(0))
     print(model)
-    a = nd.ones(1000)
-    b = nd.ones(1000)
+    model.initialize()
+    a = nd.ones([1,1000])
+    b = nd.ones([1,1000])
     model(a,b)
-    # model(nd.ones(1000),nd.ones(1000))
-
 if __name__ == '__main__':
     main()
