2511987a0de068cbdc95e776bc815fcb83c42690
wxz <876688461@qq.com>
2018-07-31 14:37:09 +0000

    nothing

--- run_and_time.sh
+++ run_and_time.sh
@@ -17,16 +17,16 @@ seed=${1:-1}
 echo "unzip ml-20m.zip"
 if unzip ml-20m.zip
 then
-    echo "Start processing ml-20m/ratings.csv"
-    t0=$(date +%s)
-	python $BASEDIR/convert.py ml-20m/ratings.csv ml-20m --negatives 999
-    t1=$(date +%s)
-	delta=$(( $t1 - $t0 ))
-    echo "Finish processing ml-20m/ratings.csv in $delta seconds"
+#    echo "Start processing ml-20m/ratings.csv"
+#    t0=$(date +%s)
+#	python3 $BASEDIR/convert.py ml-20m/ratings.csv ml-20m --negatives 999
+#   t1=$(date +%s)
+#	delta=$(( $t1 - $t0 ))
+ #   echo "Finish processing ml-20m/ratings.csv in $delta seconds"
 
     echo "Start training"
     t0=$(date +%s)
-	python $BASEDIR/ncf.py ml-20m -l 0.0005 -b 2048 --layers 256 128 64 -f 64 \
+	python3 $BASEDIR/ncf.py ml-20m -l 0.0005 -b 2048 --layers 256 128 64 -f 64 \
 		--seed $seed --threshold $THRESHOLD --processes 10
     t1=$(date +%s)
 	delta=$(( $t1 - $t0 ))
