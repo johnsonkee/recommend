db418b0bd87f91129f7cc5bc202d4a3fd98e5121
wxz <876688461@qq.com>
2018-07-31 17:19:05 +0800

    block to hybriBlock concatenate to concat, while the former one is out-of-date

--- neumf.py
+++ neumf.py
@@ -5,7 +5,7 @@ import mxnet as mx
 import pdb
 
 # in mxnet ,using mxnet.gluon.nn.Block to initiate the the deep neural network
-class NeuMF(nn.Block): #if using nn.Hybridblock, it will generate static graph
+class NeuMF(nn.HybridBlock): #if using nn.Hybridblock, it will generate static graph
     def __init__(self, nb_users, nb_items,
                  mf_dim, mf_reg,
                  mlp_layer_sizes,
@@ -37,7 +37,7 @@ class NeuMF(nn.Block): #if using nn.Hybridblock, it will generate static graph
             self.mlp_item_embed = nn.Embedding(nb_items, mlp_layer_sizes[0] // 2,
                                            weight_initializer=mx.init.Normal())
 
-            self.mlp = nn.Sequential()
+            self.mlp = nn.HybridSequential()
             for i in range(1, nb_mlp_layers):
                 self.mlp.add(nn.Dense(units=mlp_layer_sizes[i],
                                       in_units=mlp_layer_sizes[i-1],
@@ -59,12 +59,12 @@ class NeuMF(nn.Block): #if using nn.Hybridblock, it will generate static graph
         # TODO 3: is there function:cat in mxnet
         # previous one: mlp = torch.cat((xmlpu, xmlpi), dim=1)
         # use nd.concatenate([xmlpu, xmlpi], axis=0) to replace
-        xmlp = nd.concatenate([xmlpu, xmlpi], axis=1)
+        xmlp = nd.concat(xmlpu, xmlpi, dim=1)
         # axis=0 means increasing the row, while axis=1 means increasing the column
 
         xmlp = self.mlp(xmlp)
 
-        x = nd.concatenate([xmf, xmlp], axis=1)
+        x = nd.concat(xmf,xmlp, dim=1)
         x = self.final(x)
         if sigmoid:
             x = nd.sigmoid(x)
@@ -83,6 +83,7 @@ def main():
     b = nd.ones([10])
     model(a,b,True)  # 在调用model时，参数里不要带着参数名，直接写变量就好
     c = model(a,b,True)
+    c.as_in_context(mx.cpu(0)).asnumpy()
 
 
 
